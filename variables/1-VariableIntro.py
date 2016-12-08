7703902d22a7a31fb700db741f8896fd39f91a68
DJ Troutman <bakerlady622@yahoo.com>
2016-12-07 21:42:18 -0500

    my home work

--- variables/1-VariableIntro.py
+++ variables/1-VariableIntro.py
@@ -1,29 +1,5 @@
-#!/usr/bin/python
+foo = ("hello world!")
+print foo
 
-message = "What is a variable?"
-print(message)
-
-message = "Open this file in your editor to learn more!"
-
-print(message)
-
-# Because this lines starts with "#" it is a comment that python ignores.
-# Comments are important for you, the programmer.
-
-# When you are writing code, you can "comment out" a line to keep it from running:
-
-# message = "This line was commented out"
-# print(message)
-
-# Uncomment the two lines above and re-run it
-
-# In this example, we have created a variable called "message".
-
-# Look at lines 3 and 4.  We could have skipped creating the variable and just used "print()":
-
-# print("This is a print statement that doesn't use a variable")
-
-# One nice thing about variables is that we can reassign the values easily, like we did on line 6
-
-# TODO: Create a new variable and print it - When you are done, commit into git!
-
+hello = ("how are you")
+print hello
